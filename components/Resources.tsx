4a6206cfa5114104054b0ab20ef959354661dc01
Elizabeth Renderos <elirenderos@gmail.com>
2023-08-10 21:36:35 +0900

    worked on resources component

--- components/Resources.tsx
+++ components/Resources.tsx
@@ -3,13 +3,51 @@ import Image from 'next/image'
 
 const Resources = () => {
   return (
-    <div className="overflow-x-auto relative p-80 mt-40" id='teacher-resources'>
+    <div className="overflow-x-auto relative p-52 mt-40" id='teacher-resources'>
     <Image 
     src='/computer2.png'
     fill
     className='object-contain '
     alt='computer'
     /> 
+  <div className="overflow-x-auto">
+  <table className="table w-[50rem] ml-40 text-lg">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Website Link</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <td>Slidesmania</td>
+        <td>Free PPT templates and Google slides</td>
+        <td><a href='https://slidesmania.com/' target='_blank'>Click Here</a></td>
+      </tr>
+      {/* row 2 */}
+      <tr>
+        <td>Teachers Pay Teachers</td>
+        <td>PreK-12 resources</td>
+        <td><a href='https://www.teacherspayteachers.com/' target='_blank'>Click Here</a></td>
+      </tr>
+      {/* row 3 */}
+      <tr>
+        <td>Korshare</td>
+        <td>Foreign Teacher File Sharing</td>
+        <td><a href='https://korshare.jcink.net/index.php?act=idx' target='_blank'>Click Here</a></td>
+      </tr>
+       {/* row 4 */}
+       <tr>
+        <td>Twinkl</td>
+        <td>Printable and digital materials</td>
+        <td><a href='https://www.twinkl.com/' target='_blank'>Click Here</a></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 </div>
   )
 }
