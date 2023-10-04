ae72337327234630ae16bc90d822d8212101e572
Elizabeth Renderos <elirenderos@gmail.com>
2023-10-04 20:35:52 +0900

    fixed form issue

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -11,7 +11,7 @@ function Contact() {
   <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
       <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Let's Talk!</h2>
       <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a question about any resources or games? Do you want to send feedback? Let me know!</p>
-      <form  className="space-y-8">
+      <form  className="space-y-8" action="https://formcarry.com/s/z-NofMdhiD" method="POST" accept-charset="UTF-8">
           <div>
               <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
               <input type="email" id="email" name='email' className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@gameleon.com" required />
@@ -24,13 +24,7 @@ function Contact() {
               <label  htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
               <textarea id="message" name='message'  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
           </div>
-          <CustomButton 
-            title='Submit'
-            containerStyles='py-3 px-5 text-sm font-medium text-center text-white rounded-full bg-blue-400 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
-            textStyles="text-white text-[14px] leading-[17px] font-bold"
-            link=''
-            type="submit"
-          />
+          <button type="submit" className='btn bg-sky-500 text-white'>Send</button>
       </form>
   </div>
 </section>
